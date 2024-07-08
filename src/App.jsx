7d6bf4e976563961c1c3aa63cf91953e52a2435f
Aakash Brahma <Aakash.Brahma@jda.com>
2024-07-08 10:42:18 +0530

    Initial commit for pokemon React app

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import PokemonApp from './Components/PokemonApp'
+
+function App() {
+ 
+
+  return (
+    <>
+      <PokemonApp/>
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/PokemonApp.jsx
@@ -0,0 +1,99 @@
+import { useEffect, useState } from "react";
+import PokemonThumbnail from "./PokemonThumbnail";
+
+
+
+function PokemonApp(){
+    const PokemonURL="https://pokeapi.co/api/v2/pokemon?limit=20";
+    const [allPokemons,setAllPokemons]=useState([]);
+    //const [loadPoke,setLoadPoke]=useState(PokemonURL);
+
+    //my code
+    const [loadNext,setLoadNext]=useState("");
+    const [loadPrev,setLoadPrev]=useState("");
+
+    const getAllPokemons=async (url)=>{
+        let res=await fetch(url);
+        let data=await res.json();
+        console.log(data);
+        //setLoadPoke(data.next); 
+        setLoadNext(data.next);
+        setLoadPrev(data.previous);
+        setAllPokemons([]);
+         function createPokemonObject(result){
+            result.forEach(async (pokemon) => {
+                //console.log(pokemon.name);
+                const res=await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon.name}`);
+                const data=await res.json();
+                console.log(data);
+                setAllPokemons(currentList  => [...currentList ,data]);
+            });
+        }
+        createPokemonObject(data.results)
+         
+    }
+
+    useEffect(()=>{
+        getAllPokemons(PokemonURL);
+    },[])
+
+    const loadPrevious=()=>{
+        if(loadPrev!=null){
+            getAllPokemons(loadPrev);
+        }
+    }
+
+    const loadNextPage=()=>{
+        if(loadNext!=null){
+            getAllPokemons(loadNext);
+        }
+    }
+    loadNext
+
+    //console.log(allPokemons);
+
+    return (
+        <>
+            <div className="app-container">
+                <h1>Pokemon Kingdom .</h1>
+
+                <div className="pokemon-container">
+                    <div className="all-container">
+                    {allPokemons.map((pokemon,index)=> 
+                        <PokemonThumbnail
+                        id = {pokemon.id}
+                        name = {pokemon.name}
+                        image = {pokemon.sprites.other.home.front_shiny}
+                        // dream_world.front_default
+                        type={pokemon.types[0].type.name}
+                        key={index}
+                        height = {pokemon.height}
+                        weight = {pokemon.weight}
+                        stat1 = {pokemon.stats[0].stat.name}
+                        stat2 = {pokemon.stats[1].stat.name}
+                        stat3 = {pokemon.stats[2].stat.name}
+                        stat4 = {pokemon.stats[3].stat.name}
+                        stat5 = {pokemon.stats[4].stat.name}
+                        stat6 = {pokemon.stats[5].stat.name}
+                        bs1 = {pokemon.stats[0].base_stat}
+                        bs2 = {pokemon.stats[1].base_stat}
+                        bs3 = {pokemon.stats[2].base_stat}
+                        bs4 = {pokemon.stats[3].base_stat}
+                        bs5 = {pokemon.stats[4].base_stat}
+                        bs6 = {pokemon.stats[5].base_stat}
+                        
+                        />
+                    )}
+                    </div>
+                    {/* <button className="load-more" onClick={()=>getAllPokemons()}>More Pokemons</button> */}
+                    <div>
+                        <button className="load-more" onClick={()=>loadPrevious()}>Previous</button>
+                        <button className="load-more" onClick={()=>loadNextPage()}>Next</button>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default PokemonApp;
